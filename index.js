e213bee03c45957fea37c89967cc83ede8f51cc6
Denise Escobar <descobar1490@gmail.com>
2018-12-08 12:17:07 -0600

    Get request is working for projects

--- index.js
+++ index.js
@@ -9,7 +9,43 @@ const PORT = 5000;
 
 server.use(parser)
 
-//GET
+//GET Project
+
+server.get('/projects', (req, res) =>{
+    projectDb.get()
+    .then( projects =>{
+        res
+        .status(200)
+        .json(projects)
+    })
+    .catch(err =>{
+        res
+        .status(500)
+        .json({error: "The project information could not be retrieved"})
+    })
+})
+
+server.get('/projects/:id', (req, res) =>{
+    const { id } = req.params
+    console.log(id)
+    projectDb.get(id)
+    .then( project =>{
+        if(project){
+            res
+            .status(200)
+            .json(project)
+        } else {
+            res
+            .status(404)
+            .json({error: "The project with the specified if does not exist"})
+        }  
+    })
+    .catch(err =>{
+        res
+        .status(500)
+        .json({error: "The project information could not be retrieved"})
+    })
+})
 
 //POST
 
