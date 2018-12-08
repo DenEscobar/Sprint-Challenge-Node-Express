c8116c29aa30ae41f30ee5839ebece0da20e370e
Denise Escobar <descobar1490@gmail.com>
2018-12-08 13:02:53 -0600

    Put and Delete request working

--- index.js
+++ index.js
@@ -80,7 +80,58 @@ server.post('/projects', (req, res) =>{
 
 //PUT
 
+server.put('/projects/:id', (req, res) =>{
+    const  { id } = req.params
+    console.log(id)
+    const project = req.body
+    projectDb.update(id, project)
+    .then( idInfo =>{
+        console.log(idInfo)
+        if(idInfo !== null){
+            if(project.name && project.description && project.completed){
+                res
+                .status(200)
+                .json({message: "Project information updated"})
+            } else{
+                res
+                .status(400)
+                .json({errorMessage: "Please provide information to update project"})
+            }
+        } else {
+            res
+            .status(404)
+            .json({error: "The user with the specified id does not exist"})
+        }
+    })
+    .catch(err =>{
+        res
+        .status(500)
+        .json({error: "The project information could not be modified"})
+    })
+})
+
 //DELETE
+
+server.delete('/projects/:id', (req, res) =>{
+    const { id } = req.params
+    projectDb.remove(id)
+    .then(count =>{
+        if(count === 1){
+            res
+            .status(200)
+            .json({message: `Project ID:${id} deleted`})
+        } else {
+            res
+            .status(404)
+            .json({message: "The project with the specified ID does not exist"})
+        }
+    })
+    .catch( err =>{
+        res
+        .status(500)
+        .json({error: "The project could not be removed"})
+    })
+})
 
 server.listen(PORT, () =>{
     console.log(`server is running on port ${PORT}`);
